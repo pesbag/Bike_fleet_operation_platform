4a1f4f0dc45dbd5757a540b2373fe99864f9e23d
Pesach Bagad <bgdpsh@gmail.com>
2026-09-14 11:54:34 +0300

    add services folders

---
+++ Bike_fleet_operation_platform/Services/StationStatusService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_fleet_operation_platform.Services
+{
+    internal class StationStatusService
+    {
+    }
+}
