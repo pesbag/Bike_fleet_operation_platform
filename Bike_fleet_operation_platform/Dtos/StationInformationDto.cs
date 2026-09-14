10692e764351e5c571513495d8f850d7c73a89ff
Pesach Bagad <bgdpsh@gmail.com>
2026-09-14 11:53:29 +0300

    initial push

---
+++ Bike_fleet_operation_platform/Dtos/StationInformationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_fleet_operation_platform.Dtos;
+
+public class StationInformationDto
+{
+    public long station_id { get; set; }
+    public string name { get; set; } = string.Empty;
+    public double lat { get; set; }
+    public double lon { get; set; }
+    public int capacity { get; set; }
+}
---
+++ Bike_fleet_operation_platform/Dtos/StationStatusDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_fleet_operation_platform.Dtos;
+
+public class StationStatusDto
+{
+    public long station_id { get; set; }
+    public int nun_vehicles_available { get; set; }
+    public int num_docs_available { get; set; }
+    public bool is_renting { get; set; }
+    public bool is_returning { get; set; }
+    public long last_reported { get; set; }
+}
